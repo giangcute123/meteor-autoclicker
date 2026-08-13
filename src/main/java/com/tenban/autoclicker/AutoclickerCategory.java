c412f3ceed58576e34843f3292ba8ba99fe607e6
Zaoomc <dinhtruonggiang24108@gmail.com>
2026-08-14 01:03:55 +0700

    Add AutoclickerCategory class with AUTOCLICKER constant

---
+++ src/main/java/com/tenban/autoclicker/AutoclickerCategory.java
@@ -0,0 +1,7 @@
+package com.tenban.autoclicker;
+
+import meteordevelopment.meteorclient.systems.modules.Category;
+
+public class AutoclickerCategory {
+    public static final Category AUTOCLICKER = new Category("Autoclicker");
+}
